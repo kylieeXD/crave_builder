db29682a1f360d7098976cae9fe7426f714ca425
kylieeXD <khayloaftck@gmail.com>
2026-04-26 14:09:25 +0700

    arrow: switch camera vendor repo
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>

--- arrow-13.1_ext.sh
+++ arrow-13.1_ext.sh
@@ -20,7 +20,7 @@ git clone https://github.com/HinohArata/device_xiaomi_surya-arrow.git device/xia
 git clone https://github.com/HinohArata/android_vendor_xiaomi_surya.git vendor/xiaomi/surya
 git clone https://gitea.com/HinohArata/firmware_xiaomi_surya.git firmware/xiaomi/surya
 git clone https://gitlab.com/HinohArata/device_xiaomi_surya-miuicamera device/xiaomi/surya-miuicamera
-git clone https://gitlab.com/HinohArata/vendor_xiaomi_surya-miuicamera vendor/xiaomi/surya-miuicamera
+git clone https://gitlab.com/kylieeXD/vendor_xiaomi_surya-miuicamera vendor/xiaomi/surya-miuicamera
 git clone https://github.com/Cilok-LAB/android_kernel_xiaomi_surya kernel/xiaomi/surya
 git clone https://github.com/Mnzz-Prjkt/android_private_keys vendor/private/keys/keys.mk
 git clone https://github.com/HinohArata/hardware_xiaomi.git hardware/xiaomi
