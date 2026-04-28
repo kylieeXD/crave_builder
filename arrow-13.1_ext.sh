0934635b9d06d19c30af52a0821103b0f0c51fb6
kylieeXD <khayloaftck@gmail.com>
2026-04-28 23:48:46 +0700

    arrow: update vendor repository
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>

--- arrow-13.1_ext.sh
+++ arrow-13.1_ext.sh
@@ -18,7 +18,7 @@ repo sync
 
 # Clone device tree
 git clone https://github.com/Cilok-LAB/android_device_xiaomi_surya.git -b arrow-13.1 device/xiaomi/surya
-git clone https://gitlab.com/crdroidandroid/proprietary_vendor_xiaomi_surya.git -b 13.0 vendor/xiaomi/surya
+git clone https://github.com/subhajeetmuhuri/proprietary_vendor_xiaomi_surya.git -b lineage-20 vendor/xiaomi/surya
 git clone https://gitlab.com/Evolution-X-HZM/firmware_xiaomi_surya.git firmware/xiaomi/surya
 git clone https://gitlab.com/Evolution-X-HZM/device_xiaomi_surya-miuicamera.git device/xiaomi/surya-miuicamera
 git clone https://gitlab.com/Evolution-X-HZM/vendor_xiaomi_surya-miuicamera.git vendor/xiaomi/surya-miuicamera
