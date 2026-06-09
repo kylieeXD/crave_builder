8ed72a8bd02c7485ee23f9b0f442b2c48bdbadc9
khayloaf <khayloaftck@gmail.com>
2026-06-09 16:04:12 +0700

    axion: update patch URL for device/xiaomi/klee
    Signed-off-by: khayloaf <khayloaftck@gmail.com>

--- axion-16.2.sh
+++ axion-16.2.sh
@@ -3,7 +3,7 @@ for d in .repo/local_manifests kernel/xiaomi device/xiaomi device/mediatek/sepol
 for s in "$HOME/.s" "$(pwd)/.s"; do [ -f "$s" ] && source "$s" || echo ".s not found in $(dirname $s)"; done
 repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs;/opt/crave/resync.sh;repo sync --force-sync --no-clone-bundle --no-tags
 git clone https://github.com/lineageos-personal/android_packages_apps_DolbyAtmos packages/apps/DolbyAtmos;git clone https://kylieeXD:{$STGHFR}@github.com/Cilok-LAB/android_device_xiaomi_klee -b lineage-23.2 device/xiaomi/klee
-cd device/xiaomi/klee && curl -L https://pixeldrain.com/api/file/SoktJVu1 -o axion.patch && patch -p1 < axion.patch && git clean -fd && cd -
+cd device/xiaomi/klee && curl -L https://pixeldrain.com/api/file/wnW9EQQY -o axion.patch && patch -p1 < axion.patch && git clean -fd && cd -
 for lib in libncurses libtinfo; do sudo ln -sf /usr/lib/x86_64-linux-gnu/${lib}.so.6 /usr/lib/x86_64-linux-gnu/${lib}.so.5 done
 export BUILD_USERNAME=khayloaf BUILD_HOSTNAME=crave
 . build/envsetup.sh && gk -s
